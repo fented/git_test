558b5f22dd4435985fd4442b62e640624216de22
itstrenor <quitdiscord@protonmail.com>
2023-01-12 18:15:26 -0500

    added loop

--- stuff./Animal API.py
+++ stuff./Animal API.py
@@ -13,11 +13,15 @@ def get_animal_image(animal: str) -> str:
         return None
 
 def main():
-    choice = input("Enter 'cat' for cat image or 'dog' for dog image: ")
-    image_url = get_animal_image(choice)
-    if image_url:
-        print(image_url)
-    else:
-        print("Invalid choice. Please enter 'cat' or 'dog'.")
+    while True:
+        choice = input("Enter 'cat' for cat image or 'dog' for dog image: ")
+        image_url = get_animal_image(choice)
+        if image_url:
+            print(image_url)
+        else:
+            print("Invalid choice. Please enter 'cat' or 'dog'.")
+        again = input("Do you want to retrieve another image? (y/n): ")
+        if again.lower() == "n":
+            break
 
 main()
