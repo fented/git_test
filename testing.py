a64c66310c71ebdf30da8ca6d4ca2de0b2af4ff5
itstrenor <92647872+itstrenor@users.noreply.github.com>
2023-01-12 06:45:38 -0500

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,10 @@
+List = [1, 2, 3, 4, 20, 34, 7, 8, 9, 10, 11, 12]
+print("Initial List: ")
+print(List)
+
+# Removing elements from List
+# using Remove() method
+List.remove[5]
+List.remove(6)
+print("\nList after Removal of two elements: ")
+print(List)
