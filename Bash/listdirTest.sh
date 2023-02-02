76e08f58da1cea7221fa3c88dee9414e289300ce
itstrenor <quitdiscord@protonmail.com>
2023-02-02 09:37:26 -0500

    Bash script update

--- Bash/listdirTest.sh
+++ Bash/listdirTest.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 echo "running $0"
-
+echo ""
 if [ -z ${1+x} ];
     then echo "Incorrect Usage (./listdirTest directory/to/list/"
         else
-    echo "Getting content of $1:"
+    echo "cd \$1"
+    cd $1
+    echo "pwd:"
+    pwd
     echo ""
+    echo "ls \$1:"
     ls $1
 fi
