9989abeadc8266d9ba135255958fb1d6072c8f8e
itstrenor <92647872+itstrenor@users.noreply.github.com>
2023-02-02 10:20:21 -0400

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,9 +2,9 @@ List = [1, 2, 3, 4, 20, 34, 7, 8, 9, 10, 11, 12]
 print("Initial List: ")
 print(List)
 
-# Removing elements from List
-# using Remove() method
-List.remove[5]
+# Removing elements from List using Remove() method
+# "Fixed" but never tested. Too lazy.
+List.remove(5)
 List.remove(6)
 print("\nList after Removal of two elements: ")
 print(List)
